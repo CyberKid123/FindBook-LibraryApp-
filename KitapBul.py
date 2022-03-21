559c90af313be235c50959117a209c1a8d0adfa5
CyberKid123 <93266666+CyberKid123@users.noreply.github.com>
2022-03-21 19:26:02 +0300

    Add files via upload

---
+++ KitapBul.py
@@ -0,0 +1,32 @@
+"""
+Bu Bir Kutuphane Programidir.
+"""
+
+# Buraya diziler yazılacak
+k1 = []
+k2 = []
+k3 = []
+k4 = []
+k5 = []
+k6 = []
+
+
+print("Kütüphanemize Hoş Geldiniz...")
+print("Bu bilgisayar programı ile;\nBulmak istediğiniz kitabı rahatlıkla bulabilirsiniz...")
+print("Lütfen aramak istediğiniz kitabın ismini giriniz.")
+ara = input("Ara:")  # Bu satira bulunmak istenilen kitabin ismi girilir.
+
+if ara == "":
+    print("")
+elif ara == "":
+    print("")
+elif ara == "":
+    print("")
+elif ara == "":
+    print("")
+elif ara == "":
+    print("")
+elif ara == "":
+    print("")
+else:
+    print("Aradığınız kitap kütüphanemizde bulunmamaktadır...")
